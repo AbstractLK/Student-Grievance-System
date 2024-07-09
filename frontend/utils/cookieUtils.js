b9b74d51685171a1743afe30b146f802cdd30795
Kaveesha <kalshandhanasekara@gmail.com>
2024-07-09 13:34:32 +0530

    add cookie file

---
+++ frontend/utils/cookieUtils.js
@@ -0,0 +1,17 @@
+export function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+}
+
+// getCookie(name) {
+//   const cookies = document.cookie.split(';');
+//   for (let i = 0; i < cookies.length; i++) {
+//     const cookie = cookies[i].trim();
+//     if (cookie.startsWith(name + '=')) {
+//       return cookie.substring(name.length + 1);
+//     }
+//   }
+//   return null;
+// },
