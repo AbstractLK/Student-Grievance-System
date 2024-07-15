a688945f0a5af89e8ba753caf2829fb86ba93acf
Kaveesha <kalshandhanasekara@gmail.com>
2024-07-15 16:50:39 +0530

    add form validation for change password page

--- frontend/src/views/ChangePass.vue
+++ frontend/src/views/ChangePass.vue
@@ -1,13 +1,37 @@
 <script>
 
 export default {
-  name: "ChangePass"
+  name: "ChangePass",
+  data () {
+    return {
+      currentPass: '',
+      newPass: '',
+      confirmNewPass: '',
+      rules: {
+        required: value => !!value || 'Required.',
+        minLength: v => v.length >= 8 || 'Min 8 characters',
+        passwordMatch: () => this.newPass === this.confirmNewPass || 'Passwords do not match'
+      },
+      valid: false
+    };
+  },
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+      if (this.valid) {
+        this.changePassword();
+      } else alert('not valid');
+    },
+    changePassword() {
+      // Logic to change the password
+      alert('Password changed successfully!');
+    }
+  }
 }
 </script>
 
 <template>
-<!--  <h1>Change Pass</h1>-->
-
   <v-card
     class="mx-auto rounded-lg mt-8"
     max-width="900"
@@ -15,56 +39,65 @@ export default {
     title="Change password"
   >
     <v-divider></v-divider>
-    <div class="mx-16 my-3">
-    <v-container>
-      <v-row>
-        <v-col cols="3" class=""><v-label>Current Password</v-label></v-col>
-        <v-col cols="9">
-          <v-text-field
-            density="compact"
-            v-model="first"
-            color="primary"
-            label="Current Password"
-            variant="filled"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="3" class=""><v-label>Current Password</v-label></v-col>
-        <v-col cols="9">
-          <v-text-field
-            density="compact"
-            v-model="last"
-            color="primary"
-            label="New password"
-            variant="filled"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="3" class=""><v-label>Current Password</v-label></v-col>
-        <v-col cols="9">
-          <v-text-field
-            density="compact"
-            v-model="email"
-            color="primary"
-            label="Confirm New password"
-            variant="filled"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <div class="mx-16 my-3">
+        <v-container>
+          <v-row>
+            <v-col cols="3" class=""><v-label>Current Password</v-label></v-col>
+            <v-col cols="9">
+              <v-text-field
+                density="compact"
+                v-model="currentPass"
+                color="primary"
+                label="Current Password"
+                variant="filled"
+                :rules="[rules.required]"
+                type="password"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="3" class=""><v-label>New Password</v-label></v-col>
+            <v-col cols="9">
+              <v-text-field
+                density="compact"
+                v-model="newPass"
+                color="primary"
+                label="New password"
+                variant="filled"
+                :rules="[rules.required, rules.minLength]"
+                type="password"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="3" class=""><v-label>Confirm New Password</v-label></v-col>
+            <v-col cols="9">
+              <v-text-field
+                density="compact"
+                v-model="confirmNewPass"
+                color="primary"
+                label="Confirm New password"
+                variant="filled"
+                :rules="[rules.required, rules.passwordMatch]"
+                type="password"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
 
-    <v-divider></v-divider>
+        <v-divider></v-divider>
 
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="success">
-        Save
-        <v-icon icon="mdi-chevron-right" end></v-icon>
-      </v-btn>
-    </v-card-actions>
-    </div>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success" @click="validate">
+            Save
+            <v-icon icon="mdi-chevron-right" end></v-icon>
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </v-form>
+
   </v-card>
 
 <!--  <div class="ml-5 mt-5 mb-2 text-h5">Change Admin password</div>-->
