4baf043394637b37b403f35afde3cff3159eee71
Kaveesha <kalshandhanasekara@gmail.com>
2024-08-01 15:55:57 +0530

    verify validation

--- frontend/src/views/student/RegisterComplaint.vue
+++ frontend/src/views/student/RegisterComplaint.vue
@@ -19,6 +19,11 @@ export default {
       complaintDescription: null,
       anonymous: 'not anonymous',
     },
+    valid: false,
+    rules: {
+      required: v => !!v || 'This field is required',
+      yearRequired: v => v.length > 0 || 'Please select at least one year',
+    },
 
     cat1: [
       'Department Level',
@@ -47,24 +52,29 @@ export default {
 
   methods: {
     async submitComplaint() {
+      await this.$refs.form.validate()
+      if (this.valid){
+        await axios.post('http://localhost:3001/complaint/submit-complaint', this.complaint)
+          .then(response => {
+            console.log(response.data);
+            if (response.data) {
+              alert("complaint submitted!");
+              // router.push('/auth');
+            } else {
+              alert("not");
+              // location.reload();
+            }
+          })
+          .catch(error => {
+            console.error(error);
+            alert("An error occurred while submitting the complaint.");
+          });
+        location.reload();
+      } else{
+        alert("Please fill in all required fields.");
+      }
 
-      // alert('complaint');
-      await axios.post('http://localhost:3001/complaint/submit-complaint', this.complaint)
-        .then(response => {
-          console.log(response.data);
-          if (response.data) {
-            alert("complaint submitted!");
-            // router.push('/auth');
-          } else {
-            alert("not");
-            // location.reload();
-          }
-        })
-        .catch(error => {
-          console.error(error);
-          // Handle error response
-        });
-      location.reload();
+
     },
   },
 
@@ -85,126 +95,143 @@ export default {
 
     <div class="ml-5 mt-3 text-h5">Register Complaint</div>
     <v-sheet border class="rounded-lg mt-2 mx-5 px-10 py-7">
-      <v-row no-gutters class="text-center ">
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-sheet class="mx-5 px-2">
-            <v-select
-              v-model="complaint.category1"
-              :items="cat1"
-              :rules="[v => !!v || 'Item is required']"
-              label="Select Category *"
-              required
-            ></v-select>
-          </v-sheet>
-        </v-col>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-row no-gutters class="text-center ">
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-sheet class="mx-5 px-2">
+              <v-select
+                v-model="complaint.category1"
+                :items="cat1"
+                :rules="[rules.required]"
+                label="Select Category *"
+                required
+              ></v-select>
+            </v-sheet>
+          </v-col>
 
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-sheet class="mx-5 px-2">
-            <v-select
-              v-model="complaint.department"
-              :items="cat2"
-              label="Select Department"
-              required
-            ></v-select>
-          </v-sheet>
-        </v-col>
-      </v-row>
-      <v-row no-gutters class="text-center ">
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-sheet class="mx-5 px-2">
-            <v-select
-              v-model="complaint.type"
-              :items="cat3"
-              :rules="[v => !!v || 'Item is required']"
-              label="Select Type *"
-              required
-            ></v-select>
-          </v-sheet>
-        </v-col>
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-sheet class="mx-5 px-2">
-            <v-row>
-              <v-col md="3">
-                <v-checkbox
-                  v-model="complaint.year"
-                  color="red"
-                  label="year 1"
-                  value="year1"
-                  hide-details
-                ></v-checkbox>
-              </v-col>
-              <v-col md="3">
-                <v-checkbox
-                  v-model="complaint.year"
-                  color="indigo"
-                  label="year 2"
-                  value="year2"
-                  hide-details
-                ></v-checkbox>
-              </v-col>
-              <v-col md="3">
-                <v-checkbox
-                  v-model="complaint.year"
-                  color="orange"
-                  label="year 3"
-                  value="year3"
-                  hide-details
-                ></v-checkbox>
-              </v-col>
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-sheet class="mx-5 px-2">
+              <v-select
+                v-model="complaint.department"
+                :items="cat2"
+                label="Select Department"
+                required
+              ></v-select>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row no-gutters class="text-center ">
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-sheet class="mx-5 px-2">
+              <v-select
+                v-model="complaint.type"
+                :items="cat3"
+                :rules="[rules.required]"
+                label="Select Type *"
+                required
+              ></v-select>
+            </v-sheet>
+          </v-col>
 
-              <v-col md="3">
-                <v-checkbox
-                  v-model="complaint.year"
-                  color="blue"
-                  label="year 4"
-                  value="year4"
-                  hide-details
-                ></v-checkbox>
-              </v-col>
-            </v-row>
-          </v-sheet>
-        </v-col>
-      </v-row>
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-sheet class="mx-5 px-2">
+              <v-row>
+                <v-col md="3">
+                  <v-checkbox
+                    v-model="complaint.year"
+                    color="red"
+                    label="year 1"
+                    value="year1"
+                    :rules="[rules.yearRequired]"
+                    hide-details
+                  ></v-checkbox>
+                </v-col>
+                <v-col md="3">
+                  <v-checkbox
+                    v-model="complaint.year"
+                    color="indigo"
+                    label="year 2"
+                    value="year2"
+                    :rules="[rules.yearRequired]"
+                    hide-details
+                  ></v-checkbox>
+                </v-col>
+                <v-col md="3">
+                  <v-checkbox
+                    v-model="complaint.year"
+                    color="orange"
+                    label="year 3"
+                    value="year3"
+                    :rules="[rules.yearRequired]"
+                    hide-details
+                  ></v-checkbox>
+                </v-col>
 
-      <v-row no-gutters class="text-center ">
-        <v-col cols="12" md="12">
-          <v-sheet class="mx-5 px-2">
-            <v-textarea
-              v-model="complaint.complaintDescription"
-              label="Complaint Description *"
-              :rules="[v => !!v || 'Description is required']"
-              auto-grow
-              rows="5"
-              required
-            ></v-textarea>
-          </v-sheet>
-        </v-col>
-      </v-row>
+                <v-col md="3">
+                  <v-checkbox
+                    v-model="complaint.year"
+                    color="blue"
+                    label="year 4"
+                    value="year4"
+                    :rules="[rules.yearRequired]"
+                    hide-details
+                  ></v-checkbox>
+                </v-col>
+              </v-row>
+            </v-sheet>
+          </v-col>
+        </v-row>
 
-      <v-row no-gutters class="text-center mx-5">
-        <v-col md="3">
-          <v-checkbox
-            v-model="complaint.anonymous"
-            color="red"
-            label="Send as anonymous"
-            value="anonymous"
-            hide-details
-          ></v-checkbox>
-        </v-col>
-      </v-row>
-      <V-btn class="mx-16 mt-5" color="indigo-darken-3" @click="submitComplaint">Submit</V-btn>
+        <v-row no-gutters class="text-center ">
+          <v-col cols="12" md="12">
+            <v-sheet class="mx-5 px-2">
+              <v-textarea
+                v-model="complaint.complaintDescription"
+                label="Complaint Description *"
+                :rules="[rules.required]"
+                auto-grow
+                rows="5"
+                required
+              ></v-textarea>
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-row no-gutters class="text-center">
+          <v-col cols="12" md="12">
+            <v-sheet class="mx-5 px-2">
+              <v-file-input
+                label="Attach File (Image or PDF)"
+                accept="image/*,.pdf"
+              ></v-file-input>
+            </v-sheet>
+          </v-col>
+        </v-row>
+
+        <v-row no-gutters class="text-center mx-5">
+          <v-col md="3">
+            <v-checkbox
+              v-model="complaint.anonymous"
+              color="red"
+              label="Send as anonymous"
+              value="anonymous"
+              hide-details
+            ></v-checkbox>
+          </v-col>
+        </v-row>
+        <V-btn class="mx-16 mt-5" color="indigo-darken-3" @click="submitComplaint">Submit</V-btn>
+      </v-form>
     </v-sheet>
 
 
