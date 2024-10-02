2334ff1985cd7d4a1307d67e907022a34bfc04c8
Kaveesha <kalshandhanasekara@gmail.com>
2024-10-02 19:12:53 +0530

    update complaint view pages

--- frontend/src/views/admin/Closed-complaint.vue
+++ frontend/src/views/admin/Closed-complaint.vue
@@ -2,26 +2,51 @@
 
 // import Titles from "@/components/Titles.vue";
 
-import {formatDate} from "../../../utils/formatDate";
+import { formatDate } from "../../../utils/formatDate";
 import axios from "axios";
-import {getCookie} from "../../../utils/cookieUtils";
-import {jwtDecode} from "jwt-decode";
+import { getCookie } from "../../../utils/cookieUtils";
+import { jwtDecode } from "jwt-decode";
 
 export default {
   name: "closedComplaint",
   // components: {Titles},
-  data () {
+  data() {
     return {
+      type: [
+        'Admission ',
+        'Finance ',
+        'exams ',
+        'Lecture ',
+        'Timetable ',
+        'paper & Re-evaluation ',
+        'Other '
+      ],
+      selectedType: null,
       complaints: [],
       search: '',
       headers: [
         { title: 'Complain No', align: 'start', key: '_id' },
         { title: 'Complainant Email', key: 'complainantEmail' },
+        { title: 'Type', key: 'type' },
         { title: 'Reg Date', key: 'createdAt' },
         { title: 'Status', key: 'status' },
         { title: 'Action', key: 'action', sortable: false },
         { title: 'Remaining Days', key: 'remainingDays' },
       ],
+    }
+  },
+
+  computed: {
+    // Filter complaints based on selected type and search
+    filteredComplaints() {
+      return this.complaints.filter(complaint => {
+        const matchesType = this.selectedType ? complaint.type.trim().toLowerCase() === this.selectedType.trim().toLowerCase() : true;
+        const matchesSearch = this.search ? complaint.complainantEmail.includes(this.search) || complaint._id.includes(this.search) : true;
+        //console.log(matchesType, matchesSearch);
+        // console.log("complaint type: ", complaint.type.trim().toLowerCase(), "selected type: ", this.selectedType.trim().toLowerCase());
+        console.log(this.selectedType, this.search);
+        return matchesType && matchesSearch;
+      });
     }
   },
 
@@ -32,12 +57,12 @@ export default {
         const response = await axios.get('http://localhost:3001/complaint/closedComplaint');
         const token = getCookie("jwt");
         const decodedToken = jwtDecode(token);
-        if(decodedToken.role=="teacher"){
+        if (decodedToken.role == "teacher") {
           this.complaints = response.data;
-        }else if(decodedToken.role=="facultyAdmin"){
-          this.complaints = response.data.filter(complaint => complaint.remainingDays < 0 );
-        }else if(decodedToken.role=="universityAdmin"){
-          this.complaints = response.data.filter(complaint => complaint.remainingDays < -15 );
+        } else if (decodedToken.role == "facultyAdmin") {
+          this.complaints = response.data.filter(complaint => complaint.remainingDays < 0);
+        } else if (decodedToken.role == "universityAdmin") {
+          this.complaints = response.data.filter(complaint => complaint.remainingDays < -15);
         }
       } catch (error) {
         console.error(error);
@@ -56,30 +81,19 @@ export default {
     <v-card-title class="d-flex align-center pe-2 text-h5 mb-2 mt-4 ml-5">
       Closed Complaint
 
-      <v-spacer></v-spacer>
+      <!--          <v-spacer></v-spacer>-->
+      <v-label class="ml-16 pl-16">Filter</v-label>
+      <v-select clearable class="mx-5 mt-5" width="0" density="compact" label="Select Type" :items="type"
+        v-model="selectedType" variant="solo"></v-select>
 
-      <v-text-field
-        v-model="search"
-        density="compact"
-        label="Search by Email or Complaint ID"
-        prepend-inner-icon="mdi-magnify"
-        variant="outlined"
-        flat
-        hide-details
-        single-line
-      ></v-text-field>
+      <v-text-field v-model="search" density="compact" label="Search by Email or Complaint ID"
+        prepend-inner-icon="mdi-magnify" variant="outlined" flat hide-details single-line></v-text-field>
     </v-card-title>
 
     <v-divider></v-divider>
 
-    <v-data-table
-      :headers="headers"
-      :items="complaints"
-      :search="search"
-      class="mx-5"
-      height="430px"
-      fixed-header
-    >
+    <v-data-table :headers="headers" :items="filteredComplaints" :search="search" class="mx-5" height="430px"
+      fixed-header>
       <template v-slot:item.createdAt="{ item }">
         {{ formatDate(item.createdAt) }}
       </template>
@@ -88,12 +102,17 @@ export default {
         {{ item.anonymous === 'anonymous' ? 'Anonymous' : item.complainantEmail }}
       </template>
 
+      <template v-slot:item.type="{ item }">
+        {{ item.type }}
+      </template>
+
       <template v-slot:item.action="{ item }">
         <v-btn size="small" :to="`complaint-details/${item._id}`">View Details</v-btn>
       </template>
 
       <template v-slot:item.status="{ item }">
-        <v-btn size="small" color="green-lighten-1" variant="flat">{{ item.status }}</v-btn>
+        <v-btn size="small" style="width: 90px; font-size: 10px;" color="green-lighten-1" variant="flat">{{ item.status
+          }}</v-btn>
       </template>
 
       <template v-slot:item.remainingDays="{ item }">
@@ -103,6 +122,4 @@ export default {
   </v-card>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
